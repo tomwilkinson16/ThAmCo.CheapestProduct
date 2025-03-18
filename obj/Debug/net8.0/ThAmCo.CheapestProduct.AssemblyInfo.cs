03128087fbd701c511e335fff8d6ab991c8ce8f6
tomwilkinson16 <tomwilkinson622@gmail.com>
2025-03-18 16:41:58 +0000

    more changes

--- obj/Debug/net8.0/ThAmCo.CheapestProduct.AssemblyInfo.cs
+++ obj/Debug/net8.0/ThAmCo.CheapestProduct.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ThAmCo.CheapestProduct")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49b7bd3e6be108cbbd9bb6dd60f6b842a9440716")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b35625e8e5bc996953cf486e77305ebe869c365")]
 [assembly: System.Reflection.AssemblyProductAttribute("ThAmCo.CheapestProduct")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ThAmCo.CheapestProduct")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
