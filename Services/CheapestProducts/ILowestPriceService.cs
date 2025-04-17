bf216cd746dba0fd0060608125b184fed81326a9
tomwilkinson16 <tomwilkinson622@gmail.com>
2025-04-17 10:28:48 +0100

    blankv2

--- Services/CheapestProducts/ILowestPriceService.cs
+++ Services/CheapestProducts/ILowestPriceService.cs
@@ -6,6 +6,5 @@ namespace ThAmCo.CheapestProducts.Services.CheapestProduct
     {
         public Task<IEnumerable<LowestProductDto>> GetLowestPriceAsync();
         public Task<LowestProductDto> GetLowestPriceAsync(int id);
-        
     }
 }
